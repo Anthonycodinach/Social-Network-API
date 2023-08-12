224462025a05168fc3f69c2cbc8d598acecfb152
Anthony Codinach <ant@imac.lan>
2023-08-12 13:55:12 -0400

    created routes & controllers

---
+++ controllers/userController.js
@@ -0,0 +1,32 @@
+const { Users } = require('../models')
+
+module.exports = {
+    async getAllUsers(req, res) {
+        try {
+            const users = await Users.find();
+            console.log(users)
+            res.json(users)
+        }
+        catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async createUser(req, res) {
+        try {
+            const dbUserData = await Users.create(req.body);
+            res.json(dbUserData);
+        }
+        catch (err) {
+            res.status(500).json(err);
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
